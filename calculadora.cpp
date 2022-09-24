a14fdedbc3a612e01a3dcce0e9a5460aea35f685
Maribel <maribel.jaramillo.quinonez@utelvt.edu.ec>
2022-09-24 17:19:40 +0000

    cambié las variables de calculadora

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,26 +1,26 @@
 //Programa que presenta las operaciones básicas
-//Creado por Maribel Elizabeth Jaramillo Quiñónez 
+//Creado por Maribel Elizabeth Jaramillo Quiñónez
 //Fecha: 24-09-2022
 
 #include<iostream>
-using namespace std; 
+using namespace std;
 int main()
 {
-	float x,y,s,m,d,r;
+	float MEJQ_x,MEJQ_y,MEJQ_s,MEJQ_m,MEJQ_d,MEJQ_r;
 	//Ingreso de datos
-	cout<<"Ingrese en valor de x=:";
-	cin>>x;
-	cout<<"Ingrese en valor de y=:";
+	cout<<"Ingrese en valor de MEJQ_x=:";
+	cin>>MEJQ_x;
+	cout<<"Ingrese en valor de MEJQ_y=:";
 	//Operaciones 
-	cin>>y;
-	s=x+y;
-	m=x*y;
-	d=x/y;
-	r=x-y;
+	cin>>MEJQ_y;
+	MEJQ_s=MEJQ_x+MEJQ_y;
+	MEJQ_m=MEJQ_x*MEJQ_y;
+	MEJQ_d=MEJQ_x/MEJQ_y;
+	MEJQ_r=MEJQ_x-MEJQ_y;
 	//Muestra los resutaldos.
-	cout<<"Las suma de "<<x<<" + "<<y<<" = "<<s<<endl;
-	cout<<"Las multiplicacion de "<<x<<" * "<<y<<" = "<<m<<endl;
-	cout<<"Las division de "<<x<<" / "<<y<<" = "<<d<<endl;
-	cout<<"Las resta de "<<x<<" - "<<y<<" = "<<r<<endl;
+	cout<<"Las suma de "<<MEJQ_x<<" + "<<MEJQ_y<<" = "<<MEJQ_s<<endl;
+	cout<<"Las multiplicacion de "<<MEJQ_x<<" * "<<MEJQ_y<<" = "<<MEJQ_m<<endl;
+	cout<<"Las division de "<<MEJQ_x<<" / "<<MEJQ_y<<" = "<<MEJQ_d<<endl;
+	cout<<"Las resta de "<<MEJQ_x<<" - "<<MEJQ_y<<" = "<<MEJQ_r<<endl;
 	return 0;
 }
